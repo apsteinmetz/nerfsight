43dd4a6d663f618d901bff6cefb9ab0bcb62fedc
apsteinmetz <apsteinmetz@yahoo.com>
2016-10-12 17:27:20 -0400

    accurate trajectory calculations needs drag compensation

--- anglefinder.r
+++ anglefinder.r
@@ -16,3 +16,22 @@ Angle <- function(range,V=Velocity) {
   return(atan(BDrop/range)*rad2deg)
 }
 
+#angle to hit coordinate x,y
+# https://en.wikipedia.org/wiki/Trajectory_of_a_projectile
+
+angleToPoint <- function(x,y,v=Velocity){
+  q<-sqrt(v^4-g*(g*x^2+2*y*v^2))
+  t1<-atan((v^2+q)/(g*x))
+  t2<-atan((v^2-q)/(g*x))
+  return(min(t1,t2)*rad2deg)
+}
+  
+
+trajectory <- function(angle=0,v=Velocity,t=1){
+  angle<-angle*1/rad2deg
+  x<-v*t*cos(angle)
+  y<-v*t*sin(angle)-0.5*g*t^2
+  return(c(t,x,y))
+}
+  
+matrix(trajectory(angle=2,t=seq(0,1,0.1)),ncol=3)
